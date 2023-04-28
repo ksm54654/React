778cf838f96dd9b19b809818ca54744369307029
ksm54654 <ksm54654@gmail.com>
2023-04-28 15:54:54 +0900

    clock

--- src/02/MyClockTime.js
+++ src/02/MyClockTime.js
@@ -1,5 +1,13 @@
+// 1. useState import
+import { useState } from "react";
+
 const MyClockTime = () => {
-    let myTime = new Date().toLocaleTimeString();
+    // let myTime = new Date().toLocaleTimeString();
+    // 2. state변수 선언 
+    const [myTime, setMyTime] = useState()
+    // 3. 1초에 한번씩 state 변수 변경 
+    setInterval(() => setMyTime(new Date().toLocaleTimeString()), 1000);
+
     return (
         <footer>
             <h1>{myTime}</h1>
